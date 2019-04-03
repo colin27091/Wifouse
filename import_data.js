7f9709fb9fed5b421a373d6dbab69de113ced374
colin27091 <colinregourd@gmail.com>
2019-04-03 19:50:03 +0200

    inport_data.js

--- import_data.js
+++ import_data.js
@@ -1,5 +1,40 @@
-function myFunction() {
-	
-	document.getElementById("test").innerHTML = "Paragraph changed.";
-	
+
+
+$(document).ready(function() {
+
+
+    var data = loadJSON()//Recuperation des données du JSON file
+
+
+})
+
+
+async function loadJSON() {
+    var response = await fetch("bornes-wi-fi.json")
+    var str = await response.text()
+    var data = JSON.parse(str)
+    createIDB(data)
+}
+
+
+function createIDB(){
+    var idb = window.indexedDB
+    var request = idb.open("bornes")
+
+
+    request.onerror = function(event){
+        console.log("Erreur lors de l'initialisation de l'indexedDB")
+    }
+
+    request.onsuccess = function(event){
+        console.log("IndexedDB bien initialisée")
+    }
+
+    request.onupgradeneeded = function(event){
+        var db = event.target.result
+        for(var obj in data){
+            var objectStore = db.createObjectStore("bornes", { keyPath : "key"})
+        }
+        
+    }
 }
