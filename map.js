908dbfa2261b95634f76eeae71b066676bd12fc4
MofidK <mkrim.imp@gmail.com>
2019-04-08 21:01:38 +0200

    Map added and basic methods to navigate

---
+++ map.js
@@ -0,0 +1,60 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGE4ZTQwYjM2NCIsImEiOiJjanRmaXo4bHEwYmMyNGFydW5vdXIzNDZ5In0.uCRagyv40sEDNa78h4RWqg';
+
+var navigation;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [1.4437, 43.6043], // Toulouse Data GPS
+    zoom: 10
+});
+
+// var layerList = document.getElementById('menu');
+// var inputs = layerList.getElementsByTagName('input');
+
+// Switch the theme of the layers
+// function switchLayer(layer) {
+//     var layerId = layer.target.id;
+//     map.setStyle('mapbox://styles/mapbox/' + layerId + '-v9');
+// } 
+
+// for (var i = 0; i < inputs.length; i++) {
+//     inputs[i].onclick = switchLayer;
+// }
+
+// Label to Search Countries/Cities
+// Var from for start 
+
+var recherche = map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    
+    // Limit results to country
+    countries: 'fr',
+    
+    filter: function (item) {
+        
+        return item.context.map(function (i) {
+            
+            return i.text;
+        }).reduce(function (acc, cur) {
+            return acc || cur;
+        });
+        
+    }
+    
+}));
+
+// Add geolocate control to the map.
+// User location
+var userlocation = map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}),'bottom-right');
+
+// Navigation controls (Zoom and scroll bar on top right)
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
