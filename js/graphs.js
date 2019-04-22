28989e8c1e755712c02a7f196f4322fe2f28d80c
Quent120 <quentin.bayle12@orange.fr>
2019-04-22 14:52:00 +0200

    map responsive + graph.js

---
+++ js/graphs.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    Chart.defaults.global.title.display = true;
+    Chart.defaults.global.title.text = "Répartition des bornes Wi-Fi par quartier";
+
+    var ctx = document.getElementById('Charts').getContext('2d');
+		var chart = new Chart(ctx, {
+		type: 'line',
+
+        data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: '',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    	// Configuration options go here
+        options: {
+
+        }
+    });
+
+
+});
