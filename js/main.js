026afaf97da8f5340facff21d2dadaf1fbe461ed
Quent120 <quentin.bayle12@orange.fr>
2019-04-30 16:27:08 +0200

    Z-Index

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
     
-
-    openDB();// initDB -> loadJSON -> setData || chargeMap
-
     
-
+    openDB();// initDB -> loadJSON -> setData || chargeMap
+    
+    
+    
     $("#choixgraph").change(function(event){
         chargeChart();
     });
-
+    
     $("#checkboxdiv").change(function(){
         chargeChart();
     });
-
+    
     $("#geolocalisation").click(function(){
         navigator.geolocation.getCurrentPosition(function(position){
             centerOnCoord([position.coords.longitude, position.coords.latitude]);
             get5near([position.coords.latitude, position.coords.longitude]);
         });
     });
-
+    
     $('a').on('click', function(evt){
         evt.preventDefault(); 
         var target = $(this).attr('href');
@@ -27,19 +27,23 @@ $(document).ready(function(){
         .stop()
         .animate({scrollTop: $(target).offset().top}, 500 );
     });
+    
+    $("#reponse").click(function() {
+        $("#results").toggle('fast',function() {
+            $("#map").css('width', '107.1%');
+            $("#carte").toggleClass('col');
+            $("#carte").css("position","absolute");
+            $("#carte").css("z-index","1");
 
-    // $("#reponse").click(function() {
-    //     $("#results").toggle('fast',function() {
-    //         $("#map").css('width', '107.1%');
-    //         $("#carte").toggleClass('col-md-9');
-    //     });
-    // });
 
+        });
+    });
+    
     $("#research").keydown(function(event){
         if(event.keyCode == 13) {
             event.preventDefault();
             getCoordWithAddress(document.getElementById("research").value);
         }
     });
-
+    
 });  
